8360860ea45d80c3e8f53f26c20115151f443722
Abubakar Haruna Abdulmalik <abubakarsd.harunasadiq@gmail.com>
2024-10-09 12:00:37 +0100

    update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,7 @@ import Foundation
 
 import file_selector_macos
 import firebase_core
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
